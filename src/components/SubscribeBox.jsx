eef9fd1069dffa8cb3e984961d5f0df5ddc570a2
Prince Lakhwani <princelakhwani1608@gmail.com>
2026-01-31 15:28:51 +0530

    subscribe done

--- src/components/SubscribeBox.jsx
+++ src/components/SubscribeBox.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import "/src/Style/SubscribeBox.css";
 
 export default function SubscribeBox() {
-  return <button className="subscribe">🔔 Subscribe</button>;
+  const [subscribed, setSubscribed] = useState(false);
+
+  const handleSubscribe = () => {
+    setSubscribed(true);
+    alert("🎉 Subscription successful! You will now receive daily QOTD reminders.");
+  };
+
+  return (
+    <button
+      className={`subscribe ${subscribed ? "subscribed" : ""}`}
+      onClick={handleSubscribe}
+      disabled={subscribed}
+    >
+      {subscribed ? "Subscribed" : "🔔 Subscribe"}
+    </button>
+  );
 }
